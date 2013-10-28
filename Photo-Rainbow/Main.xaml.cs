e7c2888f74666132647b881d12f40c1a172877f8
Andrew Larkin <andrewlarkin2@gmail.com>
2013-10-27 23:00:50 -0400

    Clean up project
    Updated project structure, switched to use WPF, added unit test
    framework

---
+++ Photo-Rainbow/Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Photo_Rainbow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        private static PhotoServiceManager p = new PhotoServiceManager();
+
+        public Main()
+        {
+            InitializeComponent();
+
+            FlickrManager f = new FlickrManager();            
+            p.LoadManager(f);
+        }
+
+        private void Authenticate_Click(object sender, RoutedEventArgs e)
+        {
+            p.Authenticate();
+        }
+    }
+}
---
+++ Photo-Rainbow/PhotoServiceManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo_Rainbow
+{
+    class PhotoServiceManager
+    {
+        public static IAPIManager manager;
+
+        public void LoadManager(IAPIManager m)
+        {
+            manager = m;
+        }
+
+        public void Authenticate()
+        {
+            manager.Authenticate();
+        }
+    }
+}
