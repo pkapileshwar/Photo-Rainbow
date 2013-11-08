8a4400b264d46800a0a0a57818c047f458735fb4
pkapileshwar <pkapileshwar@gmail.com>
2013-11-08 14:41:19 -0500

    Storage Class
    This class will store data to .xml file

---
+++ Photo-Rainbow/Storage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Photo_Rainbow
+{
+    public class Storage
+    {
+        ImageColorData imagedata = new ImageColorData();
+        System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(imagedata.GetType());
+        System.IO.StreamWriter file = new System.IO.StreamWriter("C:\Users\praja\Documents\GitHub\Photo-Rainbow\Imagedata.xml");
+
+        writer.serializer(File, imagedata);
+        file.Close();
+    }
+}
